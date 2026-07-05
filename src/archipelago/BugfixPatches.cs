023c67d3c94cf556c7861989eb8ba3986fb252a1
Terry Hearst <thearst3rd@gmail.com>
2026-07-05 14:13:34 -0400

    Re-add bit swooshing cube animation

--- src/archipelago/BugfixPatches.cs
+++ src/archipelago/BugfixPatches.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Reflection;
 using FezEngine;
 using FezEngine.Structure;
@@ -11,17 +12,23 @@ using MonoMod.RuntimeDetour;
 
 namespace FEZAP.Archipelago
 {
-    public class BugfixPatches : GameComponent
+    public class BugfixPatches(Game game) : GameComponent(game)
     {
         private Type BitHost;
         private Hook BitUpdateHook;
+        private Hook BitTryInitializeHook;
 
         private MethodInfo BitShineOnYouCrazyDiamondsMethod;
         private FieldInfo BitCollectSoundsField;
         private FieldInfo BitTrackedBitsField;
+        private FieldInfo BitTrackedCollectsField;
+        private FieldInfo BitSolidCubesField;
         private int bitCollectedCount = 0;
 
-        public BugfixPatches(Game game) : base(game) {}
+        private Type SwooshingCube;
+        private ConstructorInfo SwooshingCubeConstructor;
+        private FieldInfo SwooshingCubeSplineField;
+        private MethodInfo SwooshingCubeDisposeMethod;
 
         [ServiceDependency]
         public IGameStateManager GameState { get; set; }
@@ -44,7 +51,15 @@ namespace FEZAP.Archipelago
             BitShineOnYouCrazyDiamondsMethod = BitHost.GetMethod("ShineOnYouCrazyDiamonds", BindingFlags.NonPublic | BindingFlags.Instance);
             BitCollectSoundsField = BitHost.GetField("CollectSounds", BindingFlags.NonPublic | BindingFlags.Instance);
             BitTrackedBitsField = BitHost.GetField("TrackedBits", BindingFlags.NonPublic | BindingFlags.Instance);
+            BitTrackedCollectsField = BitHost.GetField("TrackedCollects", BindingFlags.NonPublic | BindingFlags.Instance);
             BitUpdateHook = new Hook(BitHost.GetMethod("Update", BindingFlags.Public | BindingFlags.Instance), BitUpdateHooked);
+            // Also hook TryInitialize to not add in-progress bits and try to assemble a cube
+            BitTryInitializeHook = new Hook(BitHost.GetMethod("TryInitialize", BindingFlags.NonPublic | BindingFlags.Instance), BitTryInitializeHooked);
+            BitSolidCubesField = BitHost.GetField("SolidCubes", BindingFlags.NonPublic | BindingFlags.Instance);
+            SwooshingCube = BitHost.GetNestedType("SwooshingCube", BindingFlags.NonPublic);
+            SwooshingCubeConstructor = SwooshingCube.GetConstructor([typeof(TrileInstance), typeof(Mesh), typeof(Vector3), typeof(Quaternion)]);
+            SwooshingCubeSplineField = SwooshingCube.GetField("Spline", BindingFlags.Public | BindingFlags.Instance);
+            SwooshingCubeDisposeMethod = SwooshingCube.GetMethod("Dispose", BindingFlags.Public | BindingFlags.Instance);
         }
 
         private void BitUpdateHooked(Action<object, GameTime> original, object self, GameTime gameTime)
@@ -55,34 +70,64 @@ namespace FEZAP.Archipelago
                 return;
             }
 
-            // Reimplement a much simplified version of the method which just does animations and checks for bit collections
+            // Reimplement a simplified version of the method which just checks for bit collections and does some animations
             if (GameState.Loading || GameState.TimePaused || !CameraManager.Viewpoint.IsOrthographic())
                 return;
-            BitShineOnYouCrazyDiamondsMethod.Invoke(self, new object[] { (float)gameTime.ElapsedGameTime.TotalSeconds });
+            BitShineOnYouCrazyDiamondsMethod.Invoke(self, [(float)gameTime.ElapsedGameTime.TotalSeconds]);
+            IList TrackedCollects = (IList)BitTrackedCollectsField.GetValue(self);
             if (PlayerManager.Action != ActionType.GateWarp && PlayerManager.Action != ActionType.LesserWarp && !PlayerManager.Action.IsSwimming())
             {
                 List<TrileInstance> TrackedBits = (List<TrileInstance>)BitTrackedBitsField.GetValue(self);
                 TrileInstance collect = PlayerManager.AxisCollision[VerticalDirection.Up].Surface ?? PlayerManager.AxisCollision[VerticalDirection.Down].Surface;
-                if (collect != null && collect.Trile.ActorSettings.Type == ActorType.GoldenCube && TrackedBits.Contains(collect) && !LevelManager.Triles.Values.Any((TrileInstance x) => x.Overlaps && x.OverlappedTriles.Contains(collect) && x.Position == collect.Position))
+                if (collect != null && collect.Trile.ActorSettings.Type == ActorType.GoldenCube && TrackedBits.Contains(collect) && !LevelManager.Triles.Values.Any(x => x.Overlaps && x.OverlappedTriles.Contains(collect) && x.Position == collect.Position))
                 {
                     SoundEffect[] CollectSounds = (SoundEffect[])BitCollectSoundsField.GetValue(self);
                     CollectSounds[bitCollectedCount].Emit();
                     bitCollectedCount++;
                     if (bitCollectedCount >= 8)
                         bitCollectedCount = 0;
+                    Mesh SolidCubes = (Mesh)BitSolidCubesField.GetValue(self);
+                    object swooshingCube = SwooshingCubeConstructor.Invoke([collect, SolidCubes, new Vector3(0, 0, 0), SolidCubes.Rotation]);
+                    TrackedCollects.Add(swooshingCube);
                     GameState.SaveData.ThisLevel.DestroyedTriles.Add(collect.OriginalEmplacement);
                     GameState.SaveData.ThisLevel.FilledConditions.SplitUpCount++;
                     LevelManager.ClearTrile(collect);
                     TrackedBits.Remove(collect);
                 }
             }
+            for (int i = TrackedCollects.Count - 1; i >= 0; i--)
+            {
+                object swooshingCube = TrackedCollects[i];
+                Vector3SplineInterpolation spline = (Vector3SplineInterpolation)SwooshingCubeSplineField.GetValue(swooshingCube);
+                if (spline.Reached)
+                {
+                    TrackedCollects.RemoveAt(i);
+                    // Do we need to dispose it? The game doesn't...
+                }
+            }
             // The rest of this method isn't needed
+        }
+
+        private void BitTryInitializeHooked(Action<object> original, object self)
+        {
+            if (ArchipelagoManager.IsConnected())
+            {
+                // Clear this early
+                IList TrackedCollects = (IList)BitTrackedCollectsField.GetValue(self);
+                foreach (object swooshingCube in TrackedCollects)
+                {
+                    SwooshingCubeDisposeMethod.Invoke(swooshingCube, null);
+                }
+                TrackedCollects.Clear();
+            }
+            original(self);
         }
 
         protected override void Dispose(bool disposing)
         {
             base.Dispose(disposing);
             BitUpdateHook.Dispose();
+            BitTryInitializeHook.Dispose();
         }
     }
 }
